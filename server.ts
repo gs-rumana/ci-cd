69d5ba249e2cadc78c7cea40af697120b8ffb1b8
Gursahb Rumana <gursahb7@gmail.com>
2026-02-04 21:51:11 +0530

    add product page

--- server.ts
+++ server.ts
@@ -7,4 +7,8 @@ app.get('/', (_, res) => {
 	res.send("This is CI/CD");
 });
 
+app.get('/product', (_, res) => {
+	res.send("This is Product");
+});
+
 app.listen(port, () => console.log("Server running..."));
